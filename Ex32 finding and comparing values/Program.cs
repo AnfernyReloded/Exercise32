82c051cc29d8b86d8ae0620b251f7642d650915b
AnfernyReloded <78870003+AnfernyReloded@users.noreply.github.com>
2021-04-26 11:09:05 -0400

    Find a value in an array, dislaying value and index

---
+++ Ex32 finding and comparing values/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Linq;
+
+namespace Ex32_finding_and_comparing_values
+{
+    class Program
+    {
+
+        static public int Compare_to(int cc)
+        {
+            int[] arr = { 2, 8, 0, 24, 51 };
+
+            
+                for (int i = 0; i < arr.Length; i++)
+                {
+                    if (arr[i] == cc)
+                    {
+                        return i;
+                    }
+                }
+             
+                return -1;
+            
+        }
+
+
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("Enter a number ");
+                string userinput = Console.ReadLine();
+
+
+                int passnum;
+
+
+
+                if (!int.TryParse(userinput, out passnum))
+                {
+                    Console.WriteLine("That value cannot be found in the array");
+                    continue;
+                }
+
+                int num_out = passnum;
+                var final_output = Compare_to(passnum);
+
+                //if (passnum == false) then return " That value cannot be found in the array"
+                if (final_output == -1)
+                {
+                    Console.WriteLine("That value cannot be found in the array");
+                }
+
+                else
+                    
+                {
+                    Console.WriteLine("The value " + num_out + " can be found at index " + final_output);
+                }
+
+
+
+
+
+
+                Console.WriteLine("Would you like to continue Y or N?");
+                string user_choice = Console.ReadLine().ToLower();
+
+                if (user_choice != "y")
+                {
+                    Console.WriteLine("Goodbye!");
+                    break;
+                }
+            }
+        }
+    }
+}
